72070375bca03a3242771e2e4d7d7bcf0cb1d381
Ravinder <rkumarnirwal@gmail.com>
2021-12-05 16:49:32 +0530

    app changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 
+
 import './App.css';
-import BasicForms from './component/basicForms';
+import BasicForms from './components/basicForms.js';
+import Dashboard from './components/dashboard';
+import PieChartbasic from './components/piechartbasic';
+import Reset from './components/reset'
+import Startingpage from './components/startingpage';
+
 
 
 function App() {
-  return(
-<BasicForms/>
-  )
+  return (
+    <>
+      <Reset />,
+      <div>
+        <Dashboard />
+      </div>
+      <Startingpage />
+      <div></div>
+      {/* <PieChartbasic/>,<BasicForms/> */}
+    </>
+  );
 }
 
 export default App;
